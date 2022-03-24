2cd613750207a8f0d68b4fd9d18d9de171e4f28d
elayudante <juanma@elayudante.es>
2022-03-24 18:48:19 +0100

    actualizados los elementos Balance y IncomeExpenses con datos dinámicos

--- src/components/TRansaction.js
+++ src/components/TRansaction.js
@@ -4,7 +4,7 @@ export const TRansaction = ({ transaction }) => {
     const sign = transaction.amount < 0 ? '-' : '+';
 
     return (
-        <li className="minus">
+        <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
             {transaction.text} <span>{sign}{Math.abs(transaction.amount)}€</span><button className="delete-btn">x</button>
         </li>
     )
